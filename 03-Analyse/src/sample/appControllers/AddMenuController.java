f0a2adacce5bdb14c6410cee616fb2cf4e15414d
CodeyFlex <sej920@hotmail.com>
2019-12-11 15:10:19 +0100

    Added close functions to various buttons

--- 03-Analyse/src/sample/appControllers/AddMenuController.java
+++ 03-Analyse/src/sample/appControllers/AddMenuController.java
@@ -1,4 +1,16 @@
 package sample.appControllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddMenuController {
+    public Button addedButton = new Button();
+
+    public void addedButtonAction(ActionEvent addButtonEvent) throws IOException {
+        Stage stage = (Stage) addedButton.getScene().getWindow();
+        stage.close();
+    }
 }
--- 03-Analyse/src/sample/appControllers/DeleteController.java
+++ 03-Analyse/src/sample/appControllers/DeleteController.java
@@ -1,4 +1,22 @@
 package sample.appControllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DeleteController {
+    public Button confirmDeleteButton = new Button();
+
+    public void confirmDeleteButtonAction(ActionEvent addButtonEvent) throws IOException {
+        Stage stage = (Stage) confirmDeleteButton.getScene().getWindow();
+        stage.close();
+    }
+    public Button regretDeleteButton = new Button();
+
+    public void regretDeleteButtonAction(ActionEvent addButtonEvent) throws IOException {
+        Stage stage = (Stage) regretDeleteButton.getScene().getWindow();
+        stage.close();
+    }
 }
