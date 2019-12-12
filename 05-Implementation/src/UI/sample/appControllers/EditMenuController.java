4d6b464338fe6dcde70d44fcacbf68bf021f382b
Nikolaj Thor Christensen <nikolajdk1@gmail.com>
2019-12-12 13:25:05 +0100

    Reverting removed packages, moved them into UI package

---
+++ 05-Implementation/src/UI/sample/appControllers/EditMenuController.java
@@ -0,0 +1,16 @@
+package UI.sample.appControllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EditMenuController {
+    public Button editedButton = new Button();
+
+    public void editedButtonAction(ActionEvent addButtonEvent) throws IOException {
+        Stage stage = (Stage) editedButton.getScene().getWindow();
+        stage.close();
+    }
+}
