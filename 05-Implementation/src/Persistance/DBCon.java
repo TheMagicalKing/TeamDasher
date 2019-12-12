677bdf560b624fd502f34f38b351bcd09c59d858
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-12-12 14:19:39 +0100

    test commit to see if .gitignore works as intended

--- 05-Implementation/src/Persistance/DBCon.java
+++ 05-Implementation/src/Persistance/DBCon.java
@@ -15,7 +15,7 @@ REMEMBER TO ADD THIS FILE TO .gitignore!!!!!
 public class DBCon {
 
 private static String username = "root";
-private static String password = "";
+private static String password = "HEJMOR";
 private static String url = "jdbc:mysql://localhost/pokemon?ServerTimezone=UTC";
 private static Connection conDB;
 
