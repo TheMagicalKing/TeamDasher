118361f6149a9ce84a934aa208c83c80732eee97
CodeyFlex <sej920@hotmail.com>
2019-12-12 11:01:32 +0100

    Removed login menu

--- 03-Analyse/src/sample/Main.java
+++ 03-Analyse/src/sample/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("resources/LoginMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("resources/MainMenu.fxml"));
         primaryStage.setTitle("DasherDex");
         primaryStage.setScene(new Scene(root, 620, 500));
         primaryStage.show();
