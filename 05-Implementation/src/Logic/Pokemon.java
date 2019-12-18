33a4df0bdf3dc6fe74657a6b3a1321a9feaf8c1a
Nikolaj Thor Christensen <nikolajdk1@gmail.com>
2019-12-18 12:54:52 +0100

    Reverted tilbage til før Refacturing af UI Navn

--- 05-Implementation/src/Logic/Pokemon.java
+++ 05-Implementation/src/Logic/Pokemon.java
@@ -6,7 +6,7 @@ import javax.xml.namespace.QName;
 import java.sql.SQLException;
 
 public class Pokemon {
-
+    private  String sletdether;
     private String name;
     private int type;
     private String typeord;
